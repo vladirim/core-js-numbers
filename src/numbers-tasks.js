58b53fb01582bce19b1e96bc1d9c4b6d807acaf9
vladirim <vladimir0lomako@gmail.com>
2023-11-18 10:56:42 +0300

    feat: finish task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -620,8 +620,12 @@ function getHypotenuse(a, b) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  let count = 0;
+  for (let i = 1; i <= Math.abs(number); i += 2) {
+    count += 1;
+  }
+  return count;
 }
 
 module.exports = {
